743192534b1f90a3b1cb9d05cac9439d5f7dff51
Istvan Fodor <info@istvanfodor.com>
2025-10-23 11:35:52 -0500

    feat: fixed humble and jazzy starter Python version issue

--- jazzy.sh
+++ jazzy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+export PYVER="$(python -c 'import sys; print(f"{sys.version_info[0]}.{sys.version_info[1]}")')"
 export ROS_DISTRO=jazzy
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/python3.11/site-packages/isaacsim/exts/isaacsim.ros2.bridge/jazzy/lib 
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CONDA_PREFIX/lib/python$PYVER/site-packages/isaacsim/exts/isaacsim.ros2.bridge/jazzy/lib 
 
 "$@"
